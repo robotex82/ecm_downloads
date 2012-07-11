cecf9d05bbdc52069e38ae2784b73d36e272d10f
Roberto Vasquez Angel <robotex@robotex.de>
2012-07-11 16:25:57 +0200

    Added rails plugin skeleton.

---
+++ lib/ecm_downloads/version.rb
@@ -0,0 +1,3 @@
+module EcmDownloads
+  VERSION = "0.0.1"
+end
---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'be14d0d7f5e1846483cd3bb85677a128b162a8e7e27b85aafaa3bdce84029aa25ff16cc5d7f63d9d8a9acd3cf7164243750acd2d5bdb818ee1bffdd9459095e6'
