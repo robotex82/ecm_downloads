75110b91afb28e78dc888a71e3f5952bc16be5fe
Roberto Vasquez Angel <robotex@robotex.de>
2012-07-19 14:37:59 +0200

    Better views. Added routing helper.

--- lib/ecm/downloads/routing.rb
+++ lib/ecm/downloads/routing.rb
@@ -1,9 +1,21 @@
 module Ecm
   module Downloads
     class Routing
-      def self.routes(router)
-        router.resources :ecm_downloads_download_categories, :only => [ :index, :show ], :controller => 'ecm/downloads/download_categories'
-        router.resources :ecm_downloads_downloads, :only => [ :index, :show ], :controller => 'ecm/downloads/downloads'      
+      # Creates the routes for downloads and categories. You can pass options to
+      # specify the actions for both downloads and/or categories.
+      #
+      #   Ecm::Downloads::Routing.routes(self, { :download_category_actions => [ :show ]})
+      #
+      # This will only create the show action for download categories, but omit the index action.
+      def self.routes(router, options = {})
+        options.reverse_merge!(
+          { :download_category_actions => [:index, :show], 
+            :download_actions => [:index, :show] 
+          }
+        )
+        
+        router.resources :ecm_downloads_download_categories, :only => options[:download_category_actions], :controller => 'ecm/downloads/download_categories'
+        router.resources :ecm_downloads_downloads,:only => options[:download_actions], :controller => 'ecm/downloads/downloads'      
       end  
     end
   end
