def15e28b3b64ec2e3e5af483795cf7b14855162
Roberto Vasquez Angel <robotex@robotex.de>
2012-08-22 11:17:11 +0200

    Fixed factories.

--- spec/factories/ecm/downloads/download.rb
+++ spec/factories/ecm/downloads/download.rb
@@ -3,6 +3,6 @@ FactoryGirl.define do
     # name "#{Faker::Product.product} training"
     name "Rails user guide"
     ecm_downloads_download_category
-    asset { fixture_file_upload(Rails.root + "spec/fixtures/test.jpg", "image/jpg") }
+    asset { fixture_file_upload(Rails.root + "spec/fixtures/download/example.txt", "plain/text") }
   end
 end
